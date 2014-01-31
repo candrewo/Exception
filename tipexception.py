6243f3a56b96645fec8303d44b13962b006bc734
Andrew <caolegario@gmail.com>
2014-01-30 19:09:55 -0800

    2nd

--- tipexception.py
+++ tipexception.py
@@ -17,9 +17,16 @@ try:
 	print "Tip: ${:.2f}".format(tip_value)
 	print "Total: ${:.2f}".format(total)
 except ValueError: 
-	print """Sorry, you must supply a number for meal cost, 
-	tax and tip. Please enter a number."""
-except NameError:
-	print """Sorry, you must supply a number for meal cost, 
-	tax and tip. Please enter a number."""
-	raise
+	meal = float(raw_input("Please enter a number for meal cost: "))
+	tax = float(raw_input("Please enter a number for tax rate: "))
+	tip = float(raw_input("Please enter a number for tip: "))
+	tax_value = meal * tax
+	meal_with_tax = meal + (tax_value)
+	tip_value = tip * (meal_with_tax)
+	total = meal_with_tax + tip_value
+	calculate_meal_costs(meal,tax,tip)
+	print "Meal: ${:.2f}".format(meal)
+	print "Tax: ${:.2f}".format(tax_value)
+	print "Tip: ${:.2f}".format(tip_value)
+	print "Total: ${:.2f}".format(total)
+
