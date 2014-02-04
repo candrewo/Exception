fd27ebae7936b8850b473258d2bb4caa3c05b060
Andrew <caolegario@gmail.com>
2014-02-03 18:15:30 -0800

    3rd

--- tipexceptionif.py
+++ tipexceptionif.py
@@ -17,30 +17,54 @@ def calculate_meal_costs(meal_base, tax_rate, tip_rate):
                     tax_value=tax_value,
                     total = total)
     return meal_info
- 
-def main():
-    try:
-        meal = float(sys.argv[1])
-        tax = float(sys.argv[2])
-        tip = float(sys.argv[3])
-    except ValueError:
-    	while True:
-    	    	try:
-    		        meal = float(raw_input("Re-enter the cost of your meal: $"))
-    		        tax = float(raw_input("Re-enter tax rate as a decimal (e.g., .12, not 12%): "))
-    		        tip = float(raw_input("Reenter how much would you like to tip? (e.g., .20): "))
-    		        break
-    	        except ValueError:
-    	        	print "\nOops! That was not a valid number. Try again.\n"
 
-    	meal_info = calculate_meal_costs(meal,tax,tip)
-         
-    	print "The base cost of your meal was ${0:.2f}.".format(meal_info['meal_base'])
-    	print "You need to pay ${0:.2f} for tax.".format(meal_info['tax_value'])
-    	print "Tipping at a rate of {0}%, you should leave ${1:.2f} for a tip.".format(
-    	                                    float(100*meal_info['tip_rate']), 
-    	                                    meal_info['tax_value'])
-    	print "The grand total of your meal is ${0:.2f}.".format(meal_info['total'])
+def calcmeal(meal_base):
+    if meal_base > 0:
+        return meal_base
+    else:
+        while meal_base == 0:
+            print "The meal must be a number. Try again."
+            meal_base = float(raw_input("Please enter the cost of the meal: "))
+            break
+        return meal_base
+ 
+def calctax(tax_rate):
+    if tax_rate > 0:
+        return tax_rate
+    else:
+        while tax_rate == 0:
+            print "The tax rate must be a greater than 0 (eg. .10, .09)"
+            tax_rate = float(raw_input("Please enter the tax: "))
+            break
+        return tax_rate
+
+def calctip (tip_rate):
+    if tip_rate > 0:
+        return tip_rate
+    else:
+        while tip_rate == 0:
+            print "Don't be stingy. Tip greater than 0 as a percent (eg. .15, .20)"
+            tip_rate = float(raw_input("Please enter the tip: "))
+            break
+        return tip_rate
+	
+
+def main():
+    meal = float(sys.argv[1])
+    tax = float(sys.argv[2])
+    tip = float(sys.argv[3])
+    meal_base = calcmeal(meal)
+    tax_rate = calctax(tax)
+    tip_rate = calctip(tip)
+    meal_info = calculate_meal_costs(meal_base,tax_rate,tip_rate)
+
+    print "The base cost of your meal was ${0:.2f}.".format(meal_info['meal_base'])
+    print "You need to pay ${0:.2f} for tax.".format(meal_info['tax_value'])
+    print "Tipping at a rate of {0}%, you should leave ${1:.2f} for a tip.".format(
+                                        float(100*meal_info['tip_rate']), 
+                                        meal_info['tax_value'])
+    print "The grand total of your meal is ${0:.2f}.".format(meal_info['total'])
+
 
 if __name__ == '__main__':
     main()
